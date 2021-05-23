fa8bf5ae045815b6495cde67d805e332e8cb71c1
will <willmiller629@gmail.com>
2021-05-23 15:30:01 -0400

    generated recipe components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
 import { ListEditComponent } from './shopping-list/list-edit/list-edit.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingListComponent,
-    ListEditComponent
+    ListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
