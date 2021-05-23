27d5c36f34e5e570f28397d22bb167dec35d4ebc
will <willmiller629@gmail.com>
2021-05-23 15:43:54 -0400

    generated header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ListEditComponent } from './shopping-list/list-edit/list-edit.component
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail
     ListEditComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
