786913c90b8c12f6b32dd89ee3b10eb67a49f6bd
will <willmiller629@gmail.com>
2021-05-23 15:28:14 -0400

    generated two components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
+import { ListEditComponent } from './shopping-list/list-edit/list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ListEditComponent
   ],
   imports: [
     BrowserModule
